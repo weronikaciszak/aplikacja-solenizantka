efda8ccf9b4b4eedc8d359ee1ad7fdce55e23959
weronikaciszak <weronika.ciszak.klinika@gmail.com>
2026-08-08 22:31:40 +0200

    Refactor Flask app and update main execution

--- strona.py
+++ strona.py
@@ -145,4 +145,4 @@ threading.Thread(target=start_tunnel, daemon=True).start()
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+   app.run(host='0.0.0.0', port=5000)
